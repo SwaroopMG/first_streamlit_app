c23caa7042ff79f6cfef469b063734cfd7a0d674
SwaroopMG <73092077+SwaroopMG@users.noreply.github.com>
2023-07-12 20:35:26 +0530

    Update streamlit_app.py
    displat fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,3 +20,8 @@ fruit_to_show = my_fruit_list.loc[fruit_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruit_to_show)
+
+# new section to displat fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
